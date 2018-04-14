00fcb30acae226cef949c36501e31fbfa4090dc6
eabarroso <36744592+eabarroso@users.noreply.github.com>
2018-04-14 19:49:40 -0300

    Documentos

---
+++ Api-App/Script Tabela.sql
@@ -0,0 +1,24 @@
+USE [AppPeoplePro]
+GO
+
+/****** Object:  Table [dbo].[CalculoHistoricoLog]    Script Date: 4/14/2018 7:12:11 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[CalculoHistoricoLog](
+	[ds_Acao] [varchar](100) NULL,
+	[vl_valor] [varchar](100) NULL
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+
